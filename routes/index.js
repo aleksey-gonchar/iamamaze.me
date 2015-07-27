cdb893fa4887ac67f04777caac85603bd37dedce
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-07-27 14:35:11 +0700

    * hbs index page with meta from cfg

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express')
 var router = express.Router()
 var path = require('path')
+var frontCfg = require('konphyg')(process.cwd() + '/config')('front-end')
+
+
+router.get('/', function (req, res) {
+  res.render('index', frontCfg.meta)
+})
 
 module.exports = router
+
+
