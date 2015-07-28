f0db75a2da0ab76bc5a95a5ab9f2fae8413f2c46
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-07-28 15:01:51 +0700

    * gulp js watcher and bundle maker * express /build folder serves as /js & /styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,66 @@
 var gulp = require('gulp')
-var stylus = require('gulp-stylus')
+var gstylus = require('gulp-stylus')
+var stylus = require('stylus')
 var concat = require('gulp-concat')
 var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('autoprefixer-stylus')
+var browserSync = require('browser-sync')
 var debug = require('gulp-debug')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var gutil = require('gulp-util')
+var guglify = require('gulp-uglify')
 
-gulp.task('bundle-style', function() {
-  return gulp.src('src/front-end/styles/index.bundle.css')
+gulp.task('style', function() {
+  return gulp.src('src/front-end/styles/index.bundle.styl')
     .pipe(sourcemaps.init())
-    .pipe(stylus({
-      use: [autoprefixer()],
-      'include css': true,
-      url: { name: 'url', limit: false }
+    .pipe(gstylus({
+      use: autoprefixer(),
+      define: {
+        url: stylus.resolver()
+      }
     }))
-    .pipe(debug({title: 'stylus'}))
+    .pipe(debug())
+    .on('error', gutil.log)
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/styles/'))
+    .pipe(gulp.dest('build/styles/'))
+})
+
+gulp.task('js', function () {
+  var b = browserify({
+    entries: './src/front-end/js/index.jsx',
+    debug: true,
+    transform: [babelify],
+    require: ['jquery', 'lodash']
+  })
+
+  return b.bundle()
+    .pipe(source('index.bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(debug())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('build/js/'))
 })
 
 gulp.task('watch', function () {
-  gulp.watch(['src/front-end/**/*.css', 'src/front-end/**/*.styl'], ['bundle-style'])
+  gulp.watch(['src/front-end/**/*.css', 'src/front-end/**/*.styl'], ['style'])
+  gulp.watch(['src/front-end/**/*.jsx', 'src/front-end/**/*.js'], ['js'])
 })
+
+gulp.task('browser-sync', function () {
+  var files = [
+    'src/front-end/**/*.css',
+    'src/front-end/**/*.styl',
+    'src/front-end/**/*.js'
+  ];
+
+  browserSync.init(files, {
+    server: {
+      baseDir: './'
+    }
+  });
+});
--- src/front-end/js/index.jsx
+++ src/front-end/js/index.jsx
@@ -4,7 +4,7 @@ import jQuery from 'jquery'
 window.jQuery = jQuery // bootstrap depends on window.jQuery to be present
 import 'bootstrap'
 import React from 'react'
-import HelloWorld from './components/HelloWorld.jsx'
+import HelloWorld from './components/HelloWorld/HelloWorld.jsx'
 
 $(function () {
   React.render(<HelloWorld />, document.body)
