0466ebbef807bab47eaac481d227a99efec49b34
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-07-28 15:04:02 +0700

    * 'hello world' component rendered

--- src/front-end/js/index.jsx
+++ src/front-end/js/index.jsx
@@ -2,9 +2,9 @@
 import $ from 'jquery'
 import jQuery from 'jquery'
 window.jQuery = jQuery // bootstrap depends on window.jQuery to be present
-import 'bootstrap'
 import React from 'react'
 import HelloWorld from './components/HelloWorld/HelloWorld.jsx'
+//import 'bootstrap'
 
 $(function () {
   React.render(<HelloWorld />, document.body)
