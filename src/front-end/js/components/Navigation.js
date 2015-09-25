1cea37ee062438f66fd33a2f46838a7cc1dcd666
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-09-25 16:46:35 +0700

    * fixed .navbar-titles position for portrait mode

--- src/front-end/js/components/Navigation.js
+++ src/front-end/js/components/Navigation.js
@@ -11,42 +11,50 @@ NavItemLink.contextTypes = Object.assign(NavItemLink.contextTypes, {
 
 export default class Navigation extends React.Component {
   render () {
+    const socials = (
+      <div className='navbar-social'>
+        <a href='http://telegram.me/tuiteraz'>
+                  <span className='fa-stack fa-lg'>
+                    <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                    <i className='fa fa-paper-plane fa-stack-1x'/>
+                  </span>
+        </a>
+        <a href='https://fb.com/gonchara.net'>
+                  <span className='fa-stack fa-lg'>
+                    <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                    <i className='fa fa-facebook fa-stack-1x'/>
+                  </span>
+        </a>
+        <a href='https://ua.linkedin.com/in/alekseygonchar'>
+                  <span className='fa-stack fa-lg'>
+                    <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                    <i className='fa fa-linkedin fa-stack-1x'/>
+                  </span>
+        </a>
+        <a href='https://github.com/aleksey-gonchar'>
+                  <span className='fa-stack fa-lg'>
+                    <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                    <i className='fa fa-github-alt fa-stack-1x'/>
+                  </span>
+        </a>
+      </div>
+    )
+
     return (
       <Navbar fixedtop fluid data-class='Navigation'>
         <div className='navbar-header'>
-          <Link to='/app/about'>
-            <img src='/public/images/avatars/budva.jpg' className='navbar-avatar'/>
-          </Link>
-          <div className='navbar-titles'>
-            <Link to='/app/about' className='navbar-name'>
-              <h2>Alex Potter</h2>
+          <div className='navbar-avatar'>
+            <Link to='/app/about'>
+              <img src='/public/images/avatars/budva.jpg' />
             </Link>
-            <span className='navbar-sub-title'>full stack js developer</span>
-            <div className='navbar-social'>
-              <a href='http://telegram.me/tuiteraz'>
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
-                  <i className='fa fa-paper-plane fa-stack-1x'/>
-                </span>
-              </a>
-              <a href='https://fb.com/gonchara.net'>
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
-                  <i className='fa fa-facebook fa-stack-1x'/>
-                </span>
-              </a>
-              <a href='https://ua.linkedin.com/in/alekseygonchar'>
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
-                  <i className='fa fa-linkedin fa-stack-1x'/>
-                </span>
-              </a>
-              <a href='https://github.com/aleksey-gonchar'>
-                <span className='fa-stack fa-lg'>
-                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
-                  <i className='fa fa-github-alt fa-stack-1x'/>
-                </span>
-              </a>
+          </div>
+          <div className='navbar-titles'>
+            <div className='navbar-titles-container'>
+              <Link to='/app/about' className='navbar-name'>
+                <h2>Alex Potter</h2>
+              </Link>
+              <span className='navbar-sub-title'>full stack js developer</span>
+              {socials}
             </div>
           </div>
         </div>
