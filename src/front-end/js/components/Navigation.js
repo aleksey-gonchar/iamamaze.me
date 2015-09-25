daaba91be3e430fdd9cb73be76c43260a4c6855c
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-09-25 16:10:04 +0700

    * updated font-awesome -> 4.4.0 + social links

--- src/front-end/js/components/Navigation.js
+++ src/front-end/js/components/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 
 import { Nav, Navbar, CollapsibleNav } from 'react-bootstrap'
 import { NavItemLink } from 'react-router-bootstrap'
+import { Icon } from './helpers/FontAwesome.js'
 
 NavItemLink.contextTypes = Object.assign(NavItemLink.contextTypes, {
   router: React.PropTypes.object
@@ -14,13 +15,39 @@ export default class Navigation extends React.Component {
       <Navbar fixedtop fluid data-class='Navigation'>
         <div className='navbar-header'>
           <Link to='/app/about'>
-            <img src='/public/images/avatars/budva.jpg' className='navbar-avatar'/><br/>
+            <img src='/public/images/avatars/budva.jpg' className='navbar-avatar'/>
           </Link>
           <div className='navbar-titles'>
             <Link to='/app/about' className='navbar-name'>
               <h2>Alex Potter</h2>
             </Link>
             <span className='navbar-sub-title'>full stack js developer</span>
+            <div className='navbar-social'>
+              <a href='http://telegram.me/tuiteraz'>
+                <span className='fa-stack fa-lg'>
+                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                  <i className='fa fa-paper-plane fa-stack-1x'/>
+                </span>
+              </a>
+              <a href='https://fb.com/gonchara.net'>
+                <span className='fa-stack fa-lg'>
+                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                  <i className='fa fa-facebook fa-stack-1x'/>
+                </span>
+              </a>
+              <a href='https://ua.linkedin.com/in/alekseygonchar'>
+                <span className='fa-stack fa-lg'>
+                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                  <i className='fa fa-linkedin fa-stack-1x'/>
+                </span>
+              </a>
+              <a href='https://github.com/aleksey-gonchar'>
+                <span className='fa-stack fa-lg'>
+                  <i className='fa fa-circle fa-stack-2x fa-inverse'/>
+                  <i className='fa fa-github-alt fa-stack-1x'/>
+                </span>
+              </a>
+            </div>
           </div>
         </div>
         <div className='navbar-menu'>
