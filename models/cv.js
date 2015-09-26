ffb24790b629d4fa9d70ed46ba0e8db5db3500e9
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-09-26 19:27:37 +0700

    + cv data json

---
+++ models/cv.js
@@ -0,0 +1,231 @@
+var data = {
+  summary: '17 years of experience in software development, including 10 years in “1c” ERP development. Current tech stack includes: JavaScript, Node.js, Express.js, Mongoose, Redux+React, jQuery, LoDash/Underscore.js, HTML/HBS/Jade, Styl/CSS/LESS/SASS, Twitter-Bootstrap, WebSocket, Backbone.js, Mocha+Chai/Jasmine, Git.',
+  contacts: [
+    {
+      title: 'tuiteraz@gmail.com',
+      link: '',
+      icon: 'envelope-o'
+    },
+    {
+      title: 'Telegram',
+      link: 'http://telegram.me/tuiteraz',
+      icon: 'paper-plane'
+    },
+    {
+      title: 'Facebook',
+      link: 'https://fb.com/gonchara.net',
+      icon: 'facebook'
+    },
+    {
+      title: 'LinkedIn',
+      link: 'https://ua.linkedin.com/in/alekseygonchar',
+      icon: 'linkedin'
+    },
+    {
+      title: 'Github',
+      link: 'https://github.com/aleksey-gonchar',
+      icon: 'github-alt'
+    }
+  ],
+  languages: [
+    {
+      title: 'English',
+      rating: 4,
+      comment: '<strong>Spoken</strong>: negotiation level; <strong>Written</strong>: documentation proficient'
+    },
+    {
+      title: 'Russian',
+      rating: 5,
+      comment: '<strong>Spoken</strong>: native; <strong>Written</strong>: presentation proficient'
+    }
+  ],
+  hobbies: [
+    'Hiking', 'Sports', 'Philosophy', 'Arts', 'Dance', 'Crossfit', 'Travel'
+  ],
+  education: [
+    {
+      title: 'Elementary school',
+      details: '<u>Location:</u> Ukraine, Lugansk.',
+      year: 1985
+    },
+    {
+      title: 'Physics and Mathematics Lyceum № 24',
+      details: '<u>Location:</u> Ukraine, Lugansk.',
+      year: 1993
+    },
+    {
+      title: 'East-Ukrainian National University named Dalia',
+      details: '<u>Location:</u> Ukraine, Lugansk.',
+      year: 1996
+    },
+    {
+      title: 'MA with distinction',
+      details: '<u>Speciality:</u> engineer of hydraulic and pneumatic machines and apparatus.',
+      year: 2001
+    }
+  ],
+  skillsActualOn: '2015.6',
+  skills: [
+    {
+      title: 'Mocha<br/>+<br/>Chai<br/>/Jasmine',
+      comment: '',
+      years: 1,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'TWBP',
+      comment: 'Twitter Bootstrap',
+      years: 5,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'CSS<br/>SASS<br/>STYL<br/>LESS',
+      comment: '',
+      years: 10,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'Ember.js<br/>Backbone.js',
+      comment: '',
+      years: 1,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'Node.js<br/>+<br/>Express.js',
+      comment: '',
+      years: 5,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'React.js<br/>+<br/>Redux',
+      comment: '',
+      years: 1,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'Javascript<br/>+<br/>jQuery',
+      comment: '',
+      years: 7,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'Ruby<br/>+<br/>R\'o\'R',
+      comment: '',
+      years: 2,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'PHP',
+      comment: '',
+      years: 3,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'Git',
+      comment: '',
+      years: 5,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'HTML<br/>HBS<br/>JADE',
+      comment: '',
+      years: 10,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'MongoDB<br/>+<br/>Mongoose',
+      comment: '',
+      years: 5,
+      active: true,
+      size: 0
+    },
+    {
+      title: 'WebPack<br/>Gulp<br/>Grunt<br/>Require.js',
+      comment: '',
+      years: 4,
+      active: true,
+      size: 0
+    },
+    {
+      title: '1C 7.7',
+      comment: '',
+      years: 10,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'MySQL<br/>SQLite',
+      comment: '',
+      years: 3,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'Redis',
+      comment: '',
+      years: 1,
+      active: false,
+      size: 0
+    },
+    {
+      title: 'OAuth',
+      comment: '',
+      years: 1,
+      active: true,
+      size: 0
+    }
+  ],
+  projects: [
+    {
+      period: '2015-2015',
+      customer: 'SiftWisdom',
+      description: 'Medical professional and client collaboration platform',
+      usedSoft: 'Amazon AWS, Node.js, Express.js, React.js, getSentry, Codeship.',
+      roleDescription: 'Team leader, Chief programmer, UI/UX expert',
+      responsibilities: 'Maintain product development from scratch. Analysis and planning. Defining tech scope. Sprint planning. Job story writing task splitting and acceptance. Api development.'
+    },
+    {
+      period: '2014-2015',
+      customer: 'Elastic.io',
+      description: 'full-stack cloud service integration platform',
+      usedSoft: 'Amazon AWS, Node.js, Express.js, Ember.js, Librato, getSentry, Codeship',
+      roleDescription: 'full stack  js developer',
+      responsibilities: 'Intranet communication messages flow encryption, s3 file storage encryption. Custom API to custom API data flow configuration and implementation. Ember.js based application extension'
+    },
+    {
+      period: '2011-2013',
+      customer: 'Private investor',
+      description: 'CRM+ERP+CMS constructor that allows visually simulate arbitrary data structures and business processes. The system allows you to run from one database any shops, websites, CRM',
+      usedSoft: 'Ruby, MongoDB, Require-js, Node-js, jQuery, CoffeeScript, Styl, Twitter-Bootstrap',
+      roleDescription: 'Project manager, developer, analyst, UI/UX expert',
+      responsibilities: 'Maintain product development from scratch. Analysis and planning. Defining tech scope. Sprint planning. Job story writing task splitting and acceptance. Api development.'
+    },
+    {
+      period: '2009-2011',
+      customer: 'RinTown',
+      description: 'Automation of the enterprises operating in the field of control securities flow',
+      usedSoft: 'Windows, 1c 7.7',
+      roleDescription: 'Project manager, developer, architect',
+      responsibilities: 'Maintain product development from scratch. Analysis and planning. Defining tech scope. Sprint planning. Job story writing task splitting and acceptance. Api development.'
+    },
+    {
+      period: '2006-2008',
+      customer: 'Group of companies "Oil and Gas Systems"',
+      description: 'Development of subsystems for operational records of a large enterprise. Multi corporate and multivariate account. Managing a distributed team of developers. Conducting technical and design documentation. Simulation of logic systems and business processes of the enterprise',
+      usedSoft: 'Windows, 1c 7.7',
+      roleDescription: 'Project manager, team lead',
+      responsibilities: 'Manage local and remote development team. Analysis and design of the system.'
+    },
+  ]
+}
