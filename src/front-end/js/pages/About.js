8b25ba4ca828df33e532a56bac86f4b38cdc3593
aleksey-gonchar <gonchar.aleksey@gmail.com>
2015-09-26 14:23:54 +0700

    * what else section and links

--- src/front-end/js/pages/About.js
+++ src/front-end/js/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import Navigation from '../components/Navigation.js'
 import Footer from '../components/Footer.js'
@@ -15,6 +16,8 @@ export default class About extends React.Component {
               <p>Hi! My name is Alex Potter and I've always wanted to be a programmer. Since 1998 I cultivated this skill, constantly expanding the horizons of my knowledge and experience. I love to write beautiful, scalable, and simple code. Prefer to take on difficult, complex tasks that require non-trivial visual and architectural solutions.</p>
               <p>I enjoy working in a team, but in solo I feel myself well too. My passion for organizing and ordering information in the mind-maps allows to control large amounts of structural information. And I am easily able to find a common language with the customer and transform words and ideas in logic and relationships.</p>
               <p>I'll be glad cooperation!</p>
+              <h3>What else?</h3>
+              <p>Check my <Link to='/app/cv'>CV</Link> and then <Link to='/app/hire-me'>hire me</Link> =8)</p>
             </div>
           </div>
           <Footer />
